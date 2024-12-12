bcd767e09241e42b52181f0c9436fdf3c70f87bf
Sowndarya236 <120468745+Sowndarya236@users.noreply.github.com>
2024-12-12 18:45:05 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <h2>Contact Us</h2>
+      <p>Email: support@ecommerce.com</p>
+      <p>Phone: +9123456781</p>
+    </footer>
+  );
+}
+
+export default Footer;
